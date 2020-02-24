e0150c558cb3f5254c7e1d2d8506a8afc70635ad
Hanwen Liao <login@liaohanwen.com>
2020-02-24 15:39:55 -0500

    +

---
+++ 61.rotate-list.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=61 lang=golang
+ *
+ * [61] Rotate List
+ *
+ * https://leetcode.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (28.77%)
+ * Likes:    897
+ * Dislikes: 953
+ * Total Accepted:    240.2K
+ * Total Submissions: 831.6K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given a linked list, rotate the list to the right by k places, where k is
+ * non-negative.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL, k = 2
+ * Output: 4->5->1->2->3->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 5->1->2->3->4->NULL
+ * rotate 2 steps to the right: 4->5->1->2->3->NULL
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 0->1->2->NULL, k = 4
+ * Output: 2->0->1->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 2->0->1->NULL
+ * rotate 2 steps to the right: 1->2->0->NULL
+ * rotate 3 steps to the right: 0->1->2->NULL
+ * rotate 4 steps to the right: 2->0->1->NULL
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	
+	l := 1
+	end := head
+
+	for end.Next != nil {
+		l++
+		end = end.Next
+	}
+	
+	k %= l
+	if k == 0 {
+		return head
+	}
+
+	end.Next = head
+
+	for i := 0; i < l-k-1; i++ {
+		head = head.Next
+	}
+
+	end = head.Next
+	head.Next = nil
+	return end
+}
+// @lc code=end
+
