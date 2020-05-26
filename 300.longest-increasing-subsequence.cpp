a003540838e71e8ba0499e771ccf4e512ffce81a
Hanwen Liao <login@liaohanwen.com>
2020-05-26 13:28:27 -0400

    +

---
+++ 300.longest-increasing-subsequence.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=300 lang=cpp
+ *
+ * [300] Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (41.94%)
+ * Likes:    4259
+ * Dislikes: 101
+ * Total Accepted:    354.1K
+ * Total Submissions: 837.9K
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * Given an unsorted array of integers, find the length of longest increasing
+ * subsequence.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [10,9,2,5,3,7,101,18]
+ * Output: 4 
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+ * the length is 4. 
+ * 
+ * Note: 
+ * 
+ * 
+ * There may be more than one LIS combination, it is only necessary for you to
+ * return the length.
+ * Your algorithm should run in O(n^2) complexity.
+ * 
+ * 
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ * 
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if (it == res.end())
+                res.push_back(nums[i]);
+            else
+                *it = nums[i];
+        }
+        return res.size();
+    }
+};
+// @lc code=end
