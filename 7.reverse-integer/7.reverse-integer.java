d14b7c908487d2553c2d383abc3d1f51e6ee0913
Hanwen Liao <login@liaohanwen.com>
2021-10-03 16:16:48 +0800

    +

---
+++ 7.reverse-integer/7.reverse-integer.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (26.12%)
+ * Likes:    5744
+ * Dislikes: 8593
+ * Total Accepted:    1.8M
+ * Total Submissions: 6.9M
+ * Testcase Example:  '123'
+ *
+ * Given a signed 32-bit integer x, return x with its digits reversed. If
+ * reversing x causes the value to go outside the signed 32-bit integer range
+ * [-2^31, 2^31 - 1], then return 0.
+ * 
+ * Assume the environment does not allow you to store 64-bit integers (signed
+ * or unsigned).
+ * 
+ * 
+ * Example 1:
+ * Input: x = 123
+ * Output: 321
+ * Example 2:
+ * Input: x = -123
+ * Output: -321
+ * Example 3:
+ * Input: x = 120
+ * Output: 21
+ * Example 4:
+ * Input: x = 0
+ * Output: 0
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -2^31 <= x <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int tmp = res * 10 + x % 10;
+            if (tmp / 10 != res) {
+                return 0;
+            }
+            res = tmp;
+            x /= 10;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
