826c1b47e07ad2390e228d6d2934346cf07021ac
Hanwen Liao <login@liaohanwen.com>
2021-10-07 02:43:34 +0800

    +

---
+++ 28.implement-str-str/28.implement-str-str.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (35.72%)
+ * Likes:    2998
+ * Dislikes: 2827
+ * Total Accepted:    1M
+ * Total Submissions: 2.8M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Clarification:
+ * 
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ * 
+ * Example 1:
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * Example 2:
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * Example 3:
+ * Input: haystack = "", needle = ""
+ * Output: 0
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= haystack.length, needle.length <= 5 * 10^4
+ * haystack and needle consist of only lower-case English characters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null || needle.length() > haystack.length()) return -1;
+        
+        int[] parr = kmpPreprocess(needle);
+        int i = 0, j = 0;
+        while(i < haystack.length() && j < needle.length()) {
+            if(haystack.charAt(i) == needle.charAt(j)) {
+                i++; j++;
+            } else if(j > 0) {
+                j = parr[j - 1];
+            } else {
+                i++;
+            }
+        }
+        return j == needle.length() ? i - j : -1;
+    }
+
+    private int[] kmpPreprocess(String pattern) {
+        int i = 1, j = 0;
+        int[] res = new int[pattern.length()];
+        while(i < pattern.length()) {
+            if(pattern.charAt(i) == pattern.charAt(j)) {
+                res[i] = j+1;
+                i++; j++;
+            } else if (j > 0) {
+                j = res[j-1];
+            } else {
+                res[i] = 0;
+                i++;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
