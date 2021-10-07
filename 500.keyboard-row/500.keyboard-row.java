61272cebfebbe5eed490a77099869e0631be4774
Hanwen Liao <login@liaohanwen.com>
2021-10-08 02:06:44 +0800

    +

---
+++ 500.keyboard-row/500.keyboard-row.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=500 lang=java
+ *
+ * [500] Keyboard Row
+ *
+ * https://leetcode.com/problems/keyboard-row/description/
+ *
+ * algorithms
+ * Easy (66.88%)
+ * Likes:    741
+ * Dislikes: 819
+ * Total Accepted:    136.7K
+ * Total Submissions: 204.3K
+ * Testcase Example:  '["Hello","Alaska","Dad","Peace"]'
+ *
+ * Given an array of strings words, return the words that can be typed using
+ * letters of the alphabet on only one row of American keyboard like the image
+ * below.
+ * 
+ * In the American keyboard:
+ * 
+ * 
+ * the first row consists of the characters "qwertyuiop",
+ * the second row consists of the characters "asdfghjkl", and
+ * the third row consists of the characters "zxcvbnm".
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: words = ["Hello","Alaska","Dad","Peace"]
+ * Output: ["Alaska","Dad"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: words = ["omk"]
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: words = ["adsdf","sfd"]
+ * Output: ["adsdf","sfd"]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= words.length <= 20
+ * 1 <= words[i].length <= 100
+ * words[i] consists of English letters (both lowercase and uppercase). 
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public String[] findWords(String[] words) {
+        String[] strs = {"QWERTYUIOP","ASDFGHJKL","ZXCVBNM"};
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i<strs.length; i++){
+            for(char c: strs[i].toCharArray()){
+                map.put(c, i);
+            }
+        }
+        List<String> res = new LinkedList<>();
+        for(String w: words){
+            if(w.equals("")) continue;
+            int index = map.get(w.toUpperCase().charAt(0));
+            for(char c: w.toUpperCase().toCharArray()){
+                if(map.get(c)!=index){
+                    index = -1;
+                    break;
+                }
+            }
+            if(index!=-1) res.add(w);
+        }
+        return res.toArray(new String[0]);
+    }
+}
+// @lc code=end
+
