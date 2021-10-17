1a86be3f9e8b87029a2f9081d897505b1e6a79a4
Hanwen Liao <login@liaohanwen.com>
2021-10-17 23:32:55 +0800

    +

---
+++ 647.palindromic-substrings/647.palindromic-substrings.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=647 lang=java
+ *
+ * [647] Palindromic Substrings
+ *
+ * https://leetcode.com/problems/palindromic-substrings/description/
+ *
+ * algorithms
+ * Medium (63.50%)
+ * Likes:    5214
+ * Dislikes: 144
+ * Total Accepted:    332.2K
+ * Total Submissions: 522.5K
+ * Testcase Example:  '"abc"'
+ *
+ * Given a string s, return the number of palindromic substrings in it.
+ * 
+ * A string is a palindrome when it reads the same backward as forward.
+ * 
+ * A substring is a contiguous sequence of characters within the string.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consists of lowercase English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    int count = 0;
+
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
+        }
+
+        return count;
+    }
+
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+    }
+}
+// @lc code=end
