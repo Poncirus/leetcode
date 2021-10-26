b196dc2328dc9433d6b30977b53c93aaf504f440
Liao Hanwen <login@liaohanwen.com>
2021-10-26 15:56:10 +0800

    +

---
+++ 673.number-of-longest-increasing-subsequence/673.number-of-longest-increasing-subsequence.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=673 lang=java
+ *
+ * [673] Number of Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (39.61%)
+ * Likes:    2809
+ * Dislikes: 142
+ * Total Accepted:    91.9K
+ * Total Submissions: 231.9K
+ * Testcase Example:  '[1,3,5,4,7]'
+ *
+ * Given an integer array nums, return the number of longest increasing
+ * subsequences.
+ * 
+ * Notice that the sequence has to be strictly increasing.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,3,5,4,7]
+ * Output: 2
+ * Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and
+ * [1, 3, 5, 7].
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,2,2,2,2]
+ * Output: 5
+ * Explanation: The length of longest continuous increasing subsequence is 1,
+ * and there are 5 subsequences' length is 1, so output 5.
+ * 
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 2000
+ * -10^6 <= nums[i] <= 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length, res = 0, max_len = 0;
+        int[] len = new int[n], cnt = new int[n];
+        for (int i = 0; i < n; i++) {
+            len[i] = cnt[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (len[i] == len[j] + 1)
+                        cnt[i] += cnt[j];
+                    if (len[i] < len[j] + 1) {
+                        len[i] = len[j] + 1;
+                        cnt[i] = cnt[j];
+                    }
+                }
+            }
+            if (max_len == len[i])
+                res += cnt[i];
+            if (max_len < len[i]) {
+                max_len = len[i];
+                res = cnt[i];
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
