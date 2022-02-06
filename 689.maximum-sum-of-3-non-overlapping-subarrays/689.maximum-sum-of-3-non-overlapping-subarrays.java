51ab2bb418c69fdd51cd8a4b103f22aa484240a2
Liao Hanwen <login@liaohanwen.com>
2022-02-06 20:14:18 +0800

    +

---
+++ 689.maximum-sum-of-3-non-overlapping-subarrays/689.maximum-sum-of-3-non-overlapping-subarrays.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=689 lang=java
+ *
+ * [689] Maximum Sum of 3 Non-Overlapping Subarrays
+ *
+ * https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/description/
+ *
+ * algorithms
+ * Hard (47.97%)
+ * Likes:    1463
+ * Dislikes: 91
+ * Total Accepted:    58.3K
+ * Total Submissions: 121.5K
+ * Testcase Example:  '[1,2,1,2,6,7,5,1]\n2'
+ *
+ * Given an integer array nums and an integer k, find three non-overlapping
+ * subarrays of length k with maximum sum and return them.
+ * 
+ * Return the result as a list of indices representing the starting position of
+ * each interval (0-indexed). If there are multiple answers, return the
+ * lexicographically smallest one.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,1,2,6,7,5,1], k = 2
+ * Output: [0,3,5]
+ * Explanation: Subarrays [1, 2], [2, 6], [7, 5] correspond to the starting
+ * indices [0, 3, 5].
+ * We could have also taken [2, 1], but an answer of [1, 3, 5] would be
+ * lexicographically larger.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,2,1,2,1,2,1,2,1], k = 2
+ * Output: [0,2,4]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 2 * 10^4
+ * 1 <= nums[i] < 2^16
+ * 1 <= k <= floor(nums.length / 3)
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int n = nums.length, maxsum = 0;
+        int[] sum = new int[n + 1], posLeft = new int[n], posRight = new int[n], ans = new int[3];
+        for (int i = 0; i < n; i++)
+            sum[i + 1] = sum[i] + nums[i];
+        for (int i = k, tot = sum[k] - sum[0]; i < n; i++) {
+            if (sum[i + 1] - sum[i + 1 - k] > tot) {
+                posLeft[i] = i + 1 - k;
+                tot = sum[i + 1] - sum[i + 1 - k];
+            } else
+                posLeft[i] = posLeft[i - 1];
+        }
+        posRight[n - k] = n - k;
+        for (int i = n - k - 1, tot = sum[n] - sum[n - k]; i >= 0; i--) {
+            if (sum[i + k] - sum[i] >= tot) {
+                posRight[i] = i;
+                tot = sum[i + k] - sum[i];
+            } else
+                posRight[i] = posRight[i + 1];
+        }
+        for (int i = k; i <= n - 2 * k; i++) {
+            int l = posLeft[i - 1], r = posRight[i + k];
+            int tot = (sum[i + k] - sum[i]) + (sum[l + k] - sum[l]) + (sum[r + k] - sum[r]);
+            if (tot > maxsum) {
+                maxsum = tot;
+                ans[0] = l;
+                ans[1] = i;
+                ans[2] = r;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
